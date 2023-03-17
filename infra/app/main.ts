defa8780f944b1549e9b0ad644104543c28ecde1
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-03-18 00:39:53 +0100

    import fix

--- infra/app/main.ts
+++ infra/app/main.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
+import { App } from 'aws-cdk-lib';
 import { RsiBotStack } from '../stacks/rsi-bot-stack';
 import { rsiBotStackProps, stackName, env } from '../config';
 
-const app = new cdk.App();
+const app = new App();
 
 new RsiBotStack(app, stackName, {
   ...rsiBotStackProps,
