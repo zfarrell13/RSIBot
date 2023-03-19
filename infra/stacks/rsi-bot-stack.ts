f2c87659c15a501f8408b1f46970af92f6502ce3
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-03-19 18:57:00 +0100

    add rsi optimizations function

--- infra/stacks/rsi-bot-stack.ts
+++ infra/stacks/rsi-bot-stack.ts
@@ -1,6 +1,7 @@
-import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 
 interface RsiBotStackProps extends StackProps {
   bucketName: string;
@@ -12,15 +13,30 @@ export class RsiBotStack extends Stack {
     super(scope, id, props);
 
     this.bucket = this._createS3Bucket(props.bucketName);
+    this._createRsiOptimizationsLambda();
   }
 
-  _createS3Bucket(bucketName: string) {
+  _createS3Bucket(bucketName: string): Bucket {
     const s3Bucket = new Bucket(this, 'rsi-bot-bucket', {
       bucketName,
       removalPolicy: RemovalPolicy.RETAIN,
     });
 
     return s3Bucket;
+  }
+
+  _createRsiOptimizationsLambda(): void {
+    const rsiOptimizationsFunction = new Function(
+      this,
+      'rsi-optimizations-function',
+      {
+        runtime: Runtime.PYTHON_3_9,
+        handler: 'rsi_optimizations.lambda_handler',
+        code: Code.fromAsset('lambdas'),
+        memorySize: 256,
+        timeout: Duration.minutes(10),
+      }
+    );
   }
 }
 
