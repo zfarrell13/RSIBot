46ab873d6688c5e417027390de8e3cc26bd51812
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-03-29 22:07:00 +0200

    clean optimization lambda

--- infra/lambdas/rsi_optimizations.py
+++ infra/lambdas/rsi_optimizations.py
@@ -5,19 +5,11 @@ import numpy as np
 import ta
 import alpaca_trade_api as api
 import datetime
-# import smtplib
-# from email.mime.text import MIMEText
-
 
 s3 = boto3.client('s3')
 
 API_KEY = os.environ['API_KEY']
 SECRET_KEY = os.environ['SECRET_KEY']
-# smtp_port = os.environ['smtp_port']
-# smtp_server = os.environ['smtp_server']
-# sender_email = os.environ['sender_email']
-# sender_password = os.environ['sender_password']
-# recipient_email = os.environ['recipient_email']
 
 alpaca = api.REST(API_KEY, SECRET_KEY)
 
@@ -87,42 +79,11 @@ def optimize_rsi_thresholds(symbol):
 
     return optimal_overbought, optimal_oversold, optimal_window
 
-# def invoke_another_lambda(optimal_values, symbols):
-#     lambda_client = boto3.client('lambda')
-
-#     payload = {
-#         'optimal_values': optimal_values,
-#         'symbols': symbols
-#     }
-
-#     response = lambda_client.invoke(
-#         FunctionName='<ANOTHER_LAMBDA_FUNCTION_NAME>',
-#         InvocationType='RequestResponse', # Or Event
-#         Payload=json.dumps(payload)
-#     )
-
-#     return response
-
 def lambda_handler(event, context):
     optimal_values = {}
     for symbol in symbols:
         optimal_overbought, optimal_oversold, optimal_window = optimize_rsi_thresholds(symbol)
         optimal_values[symbol] = (optimal_overbought, optimal_oversold, optimal_window)
-
-    # message = f"Here are todays optimal_overbought, optimal_oversold, optimal_window values for {symbols}. {optimal_values}"
-
-    # msg = MIMEText(message)
-    # msg['Subject'] = 'Alpaca Bot Notification'
-    # msg['From'] = sender_email
-    # msg['To'] = recipient_email
-
-    # server = smtplib.SMTP(smtp_server, smtp_port)
-    # server.starttls()
-    # server.login(sender_email, sender_password)
-    # server.sendmail(sender_email, recipient_email, msg.as_string())
-    # server.quit()
-
-    # invoke_another_lambda(optimal_values, symbols)
 
     # Serialize the dictionary to a JSON formatted string
     json_data = json.dumps(optimal_values)
@@ -135,6 +96,7 @@ def lambda_handler(event, context):
     bucket_name = 'rsi-bot-data-bucket'
     s3.upload_file('/tmp/optimal_values.json', bucket_name, 'optimal_values.json')
 
+    print(optimal_values)
     # Return a message with the status
     return {
         'statusCode': 200,
